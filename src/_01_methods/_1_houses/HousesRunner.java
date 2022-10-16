68f6e9c6e3df0ca9827c68a42ef2df26ca582b9a
AmtrakFTW <88908613+JeoffraRi@users.noreply.github.com>
2022-10-16 14:59:55 -0700

    i finished at 2:59 pm pst
    yaaay! :sunglasses:

--- src/_01_methods/_1_houses/HousesRunner.java
+++ src/_01_methods/_1_houses/HousesRunner.java
@@ -1,5 +1,7 @@
 package _01_methods._1_houses;
 
+import java.util.Random;
+
 import org.jointheleague.graphical.robot.Robot;
 
 /*
@@ -9,59 +11,60 @@ import org.jointheleague.graphical.robot.Robot;
 public class HousesRunner {
 
 	public static void main(String[] args) {
-		//Here we call the run() method from the Houses class
-		//Do not add any more code here!
-		for(int a=0; a<10; a++) {
-			drawFlatHouse();
+		Robot rob = new Robot();
+		int height = 0;
+		int houseX = 5;
+		int houseY = 800;
+		rob.setX(houseX);
+		rob.setY(houseY);
+		// Here we call the run() method from the Houses class
+		// Do not add any more code here!
+		for (int a = 0; a < 10; a++) {
+			Random ran = new Random();
+			height = ran.nextInt(900);
+			drawFlatHouse(rob, height);
 		}
 
 	}
-	
-public static void drawFlatHouse() { 
+
+	public static void drawFlatHouse(Robot rob, int height) {
 		Houses houseBuilder = new Houses();
 		houseBuilder.run();
-		//1. Have a robot start on the left side of the window near the bottom. 
-		Robot rob = new Robot();
-		int houseX = 100;
-		int houseY = 450;
-		rob.setX(houseX);
-		rob.setY(houseY);
+		// 1. Have a robot start on the left side of the window near the bottom.
+
 		rob.penDown();
-		rob.setSpeed(100);
+		rob.setSpeed(58);
 		rob.setPenColor(219, 198, 127);
 		rob.setPenWidth(10);
-	//2. Draw a flat-topped house of height 100 with grass after it (see picture). 
-		rob.move(100);
+		// 2. Draw a flat-topped house of height 100 with grass after it (see picture).
+		rob.move(height);
 		rob.turn(90);
 		rob.move(100);
 		rob.turn(90);
-		rob.move(100);
+		rob.move(height);
 		rob.turn(-90);
 		rob.setPenColor(33, 77, 0);
 		rob.move(100);
-		rob.turn(90);
-	
-	//3. Extract the piece of code that draws the house into a method. Draw 10 houses. 
-		
-		//4. Change the method to take (int height) as a parameter. Draw 9 houses of different heights 
-			
-		//5. Make the method take a String instead of an int. 
-			
-		      //“small”                 60 
-				
-		      //“medium”             120 
-				
-		      //“large”                 250 
-				
-		//6. Make the method take a color as well as a height. The houses are drawn in that color. 
-			
-		//[optional] Set the scene to night by setting the background to black 
-			
-		//7. Give the houses peaked roofs 
-			
-		//8. Extract that roof code into a method “drawPointyRoof” and create a new method: “drawFlatRoof”. 
-			
-		//9. Make large houses have flat roofs
-		
+		rob.turn(-90);
+
+
+	}
+	public static void drawFlatHouse(Robot rob, String height) {
+		if (height.equalsIgnoreCase("small")) {
+			drawFlatHouse(rob, 60);
+		}else if (height.equalsIgnoreCase("medium")) {
+			drawFlatHouse(rob, 120);	
+		}else if (height.equalsIgnoreCase("large")) {
+			drawFlatHouse(rob, 250);
 		}
+	}
+	public static void drawColorHouse(Robot rob, String height, String color) {
+		if (color.equalsIgnoreCase("light beige")) {
+			drawFlatHouse(rob, 60);
+		}else if (color.equalsIgnoreCase("beige")) {
+			drawFlatHouse(rob, 120);	
+		}else if (color.equalsIgnoreCase("dark beige")) {
+			drawFlatHouse(rob, 250);
+		}
+	}
 }
