4398e59c9bdf62d3234b37942c70657bd9781c27
Student <league@iMac-32.attlocal.net>
2023-08-13 14:02:06 -0700

    commit halfway
    i made button!

---
+++ src/_04_gui_from_scratch/_4_cuteness_tv/CUTE.java
@@ -0,0 +1,66 @@
+package _04_gui_from_scratch._4_cuteness_tv;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URI;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class CUTE implements ActionListener {
+	
+	
+	public static void main(String[] args) {
+		CUTE cute = new CUTE();
+	}
+	
+	
+	CUTE(){
+		
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		JButton button = new JButton();
+		button.addActionListener(this);
+		panel.add(button);
+		frame.pack();
+	}
+	
+	void showDucks() {
+	     playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+	}
+
+	void showFrog() {
+	     playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+	}
+
+	void showFluffyUnicorns() {
+	     playVideo("https://www.youtube.com/watch?v=HXMrrjfUKR4");
+	}
+
+	void playVideo(String videoID) {
+	     
+	     // Workaround for Linux because "Desktop.getDesktop().browse()" doesn't
+	     //work on some Linux implementations
+	     try {
+	     if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+	     if (Runtime.getRuntime().exec(new String[] {
+	      "which", "xdg- open" }).getInputStream().read() != -1) {
+	     Runtime.getRuntime().exec(new String[] { "xdg-open", videoID });
+	     }
+	     } else {
+	          URI uri = new URI(videoID);
+	          java.awt.Desktop.getDesktop().browse(uri);
+	     }      } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		System.out.println("this button works");
+		
+	}
+}
